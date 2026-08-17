6e9595bec84ac75bf71ca4ef74d73f60368eb919
Ruby <justruru04@icloud.com>
2026-08-17 13:53:59 +0800

    feat: 完成 ChromaDB 向量庫建立與檢索

--- RAG_demo/chunking.py
+++ RAG_demo/chunking.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
-def get_split_documents(file_path: str = "data/Mimikyu.txt", encoding = 'utf-8') -> list[Document]:
+def get_split_documents(file_path: str, encoding = 'utf-8') -> list[Document]:
     loader = TextLoader(file_path, encoding = encoding)
     raw_docs = loader.load()
 
@@ -19,8 +19,11 @@ def get_split_documents(file_path: str = "data/Mimikyu.txt", encoding = 'utf-8')
     split_docs = text_splitter.split_documents(raw_docs)
     return split_docs
 
-chunks = get_split_documents()
+chunks = get_split_documents("data/Mimikyu.txt")
 
-print(f"總共有 {len(chunks)} 個 chunks")
-print("第一個 chunk 內容：", chunks[0].page_content)
-print("第一個 chunk metadata：", chunks[0].metadata)
+
+
+if __name__ == "__main__":
+    print(f"總共有 {len(chunks)} 個 chunks")
+    print("第一個 chunk 內容：", chunks[0].page_content)
+    print("第一個 chunk metadata：", chunks[0].metadata)
---
+++ RAG_demo/text_embedding.py
@@ -0,0 +1,35 @@
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from chunking import get_split_documents
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+split_docs = get_split_documents("data/Mimikyu.txt")
+embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
+
+vectorstore = Chroma.from_documents(
+    documents=split_docs,
+    embedding=embeddings,
+    persist_directory='./chroma_db',
+    collection_name='pokemon_mimikyu'
+)
+
+print(f"\n文件向量化已完成，{len(split_docs)} 個 chunks 已儲存至 ChromaDB")
+
+# 方法 1：基礎相似度檢索
+query = "Mimikyu 為甚麼晚上才會出現？"
+similar_docs = vectorstore.similarity_search(query, k=2) # k=2 代表取最相關的前 2 筆
+
+print(f"針對問題『{query}』找到的相關文件：")
+for i, doc1 in enumerate(similar_docs):
+    print(f"\n結果 {i+1} 來源: {doc1.metadata['source']})")
+    print(f"內文: {doc1.page_content}")
+
+
+# 方法 2 ：分數越小通常代表越相似
+results_with_scores = vectorstore.similarity_search_with_score(query, k=1)
+doc2, score = results_with_scores[0]
+print(f"\n最相似段落內容：{doc2.page_content}")
+print(f"\n最相似段落分數：{score:.4f}")
